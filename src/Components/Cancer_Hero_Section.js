5cd77de8ca0a06a3d12a81dab8ca4e6c6500ea9e
HamzaChaudhary786 <hmzsattar99@gmail.com>
2022-12-15 15:13:21 +0500

    first commit

--- src/Components/Cancer_Hero_Section.js
+++ src/Components/Cancer_Hero_Section.js
@@ -3,7 +3,7 @@ import { useGlobalContext } from './Context'
 import { Link } from 'react-router-dom';
 
 const Cancer_Hero_Section = () => {
-    const { fund, query_cancer, setQuery_Cancer } = useGlobalContext();
+    const { fund, query_cancer } = useGlobalContext();
 
     return (
         <>
@@ -17,7 +17,7 @@ const Cancer_Hero_Section = () => {
                                 return (
                                     <>
                                         {
-                                            names[2] == query_cancer[2] ?
+                                            names.toLowerCase().slice(0,2) == query_cancer.toLowerCase().slice(0,2) || title.toLowerCase().slice(0,2) == query_cancer.toLowerCase().slice(0,2)  ?
 
                                                 <Link>
                                                     <section key={id} className="mt-2 p-2 lg:mb-0 rounded-t-2xl  mb-2 border  cursor-pointer lg:h-[470px] lg:w-[360px] lg:ml-24 bg-gradient-to-r from-green-500 to-green-300 via-green-200 hover:from-green-500 hover:to-green-700  rounded-2xl">
@@ -40,7 +40,7 @@ const Cancer_Hero_Section = () => {
                                                 </Link>
 
                                                 :
-                                                console.log(names)
+                                                console.log(names )
                                         }
                                     </>
                                 )
--- src/Components/Flood_Hero_Section.js
+++ src/Components/Flood_Hero_Section.js
@@ -17,7 +17,7 @@ const Flood_Hero_Section = () => {
                 return (
                   <>
                     {
-                      names[2] == query[2] ?
+                       names.toLowerCase().slice(0,2) == query.toLowerCase().slice(0,2) || title.toLowerCase().slice(0,2) == query.toLowerCase().slice(0,2)  ?
 
                         <Link>
                           <section key={id} className="mt-2 p-2 lg:mb-0 rounded-t-2xl  mb-2 border  cursor-pointer lg:h-[470px] lg:w-[360px] lg:ml-24 bg-gradient-to-r from-green-500 to-green-300 via-green-200 hover:from-green-500 hover:to-green-700  rounded-2xl">
